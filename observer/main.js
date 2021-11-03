1dff50ee5ada8358ded6c9146fbd1664ebcd4345
Nicolás Álvarez Tobón <49601650+NICOLASTOBON@users.noreply.github.com>
2021-11-03 04:03:15 +0000

    [add] class

--- observer/main.js
+++ observer/main.js
@@ -14,3 +14,46 @@ class Subject {
         })
     }
 }
+
+class ItemSubject extends Subject {
+    constructor() {
+        super()
+        this.items = []
+    }
+    notify(item) {
+        this.items.push(item)
+        super.notify(this)
+    }
+}
+
+class ListObserver {
+    notify(subject) {
+        console.log('Observer List')
+        subject.items.forEach(elem => console.log(elem))
+    }
+}
+
+
+class TotalAmout {
+    notify(subject) {
+        const total = subject.items.reduce((acc, total) => acc + total.amout, 0)
+        console.log(total)
+    }
+}
+
+
+const famous = new ItemSubject()
+const fan1 = new ListObserver()
+const fan2 = new ListObserver()
+
+const total = new TotalAmout()
+
+famous.suscribe(fan1);
+famous.suscribe(fan2);
+
+famous.suscribe(total);
+
+famous.notify({
+    post: 'Hello People',
+    amout: 40
+})
